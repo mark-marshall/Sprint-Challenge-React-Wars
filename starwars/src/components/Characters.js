b17a831db83ca52055c19a4dfb38c62c37dc931f
mark-marshall <mark.marshallgp@gmail.com>
2019-02-15 08:33:41 +0000

    [MVP] Created character bio with list of items

--- starwars/src/components/Characters.js
+++ starwars/src/components/Characters.js
@@ -4,7 +4,20 @@ export default function Characters ({ starWarsData }) {
     return (
        <div className="charcer-bio-container">
        {
-           starWarsData.map(char => <h1 key={char.name}>{char.name}</h1>)
+           starWarsData.map(char => 
+           <div key={char.name}>
+               <h1 >{char.name}</h1>
+               <div className="bio-pic"></div>
+               <ul>
+                <li>Born: {char.birth_year}</li>
+                <li>Gender: {char.gender}</li>
+                <li>Height: {char.height}</li>
+                <li>Mass: {char.mass}</li>
+                <li>Eye color: {char.eye_color}</li>
+                <li># Starships: {char.starships.length}</li>
+                <li># Vehicles: {char.vehicles.length}</li>
+               </ul>
+               </div>)
        }
        </div> 
     )
