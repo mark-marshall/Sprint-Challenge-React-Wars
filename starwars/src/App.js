8ef5b5e8c58912e62a0be1b4ef83fb2501e08ead
mark-marshall <mark.marshallgp@gmail.com>
2019-02-15 08:46:25 +0000

    [MVP] Complete

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
       <div className="App">
         <h1 className="Header">React Wars</h1>
         <Characters starWarsData =  {this.state.starwarsChars}/>
+        <p><span>✨</span></p>
       </div>
     );
   }
