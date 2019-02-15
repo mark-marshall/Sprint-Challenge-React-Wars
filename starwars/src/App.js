3b3d3d46945066bb24dd51cb662f1505ce4a4756
mark-marshall <mark.marshallgp@gmail.com>
2019-02-15 10:16:30 +0000

    [STRETCH] Added button styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -6,19 +6,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: [], currentURL: "https://swapi.co/api/people/", nextURL: '', prevURL: '',
+      starwarsChars: [],
+      currentURL: 'https://swapi.co/api/people/',
+      nextURL: '',
+      prevURL: ''
     };
   }
 
   updatePageURL(button) {
-    if (button){
+    if (button) {
       this.setState({
-        currentURL: this.state.nextURL,
-      })
+        currentURL: this.state.nextURL
+      });
     } else {
       this.setState({
-      currentURL: this.state.prevURL,
-    })
+        currentURL: this.state.prevURL
+      });
     }
     this.getCharacters(this.state.currentURL);
   }
@@ -27,10 +30,7 @@ class App extends Component {
     this.getCharacters(this.state.currentURL);
   }
 
-  getCharacters = (URL) => {
-    // feel free to research what this code is doing.
-    // At a high level we are calling an API to fetch some starwars data from the open web.
-    // We then take that data and resolve it our state.
+  getCharacters = URL => {
     fetch(URL)
       .then(res => {
         return res.json();
@@ -49,10 +49,20 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
-        <Characters starWarsData =  {this.state.starwarsChars}/>
-        <button className="button" onClick={() => this.updatePageURL()}>PREVIOUS</button>
-        <button className="button" onClick={() => this.updatePageURL('next')}>NEXT</button>
-        <p><span role="img" aria-label="stars">✨</span></p>
+        <div className="button-container">
+          <button className="button" onClick={() => this.updatePageURL()}>
+            PREVIOUS
+          </button>
+          <button className="button" onClick={() => this.updatePageURL('next')}>
+            NEXT
+          </button>
+        </div>
+        <Characters starWarsData={this.state.starwarsChars} />
+        <p>
+          <span role="img" aria-label="stars">
+            ✨
+          </span>
+        </p>
       </div>
     );
   }
